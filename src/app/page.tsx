7bdf71f8ce69cf8d155bdd69b6e3f3d5a236a159
Josep Garcia <josepgs12@gmail.com>
2024-04-18 19:05:22 +0200

    padding added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import TrackedProducts from "./components/TrackedProducts";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center gap-10 min-h-screen py-20 px-15 max-w-[800px] mx-auto">
+    <main className="flex flex-col items-center gap-10 min-h-screen py-20 px-10 max-w-[800px] mx-auto">
       <h1 className="text-4xl">Xuscraper</h1>
       <p className="self-start text-xl">
         Copia y pega la URL de un producto de Amazon para empezar a trackear los precios de un producto de Amazon.
